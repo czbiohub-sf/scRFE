16f9754d9850e4fb8f46451c2d9408644dba0ffc
Madeline Park <madelinepark12@gmaill.com>
2020-06-03 11:41:22 -0700

    moved old code

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name = "scRFE",
-    version = "0.4",
+    version = "0.5",
     keywords = ("pip", "single cell", "scRFE"),
     description = "Single-cell identity definition using random forest modelling and recursive feature elimination",
-    long_description = "Single-cell identity definition using random forest modelling and recursive feature elimination",
+    long_description = "Single-cell identity one vs all classification using random forest modelling and recursive feature elimination",
     license = "MIT Licence",
 
     url = "https://github.com/czbiohub/scRFE",
