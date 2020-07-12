8b6389f3c51b8072fb2eff606cac6416606f59d6
Madeline Park <madelinepark12@gmaill.com>
2020-07-12 14:33:44 -0700

    added scRFEimplot

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'scRFE',         # How you named your package folder (MyLib)
   packages = ['scRFE'],   # Chose the same as "name"
-  version = '1.4.6',      # Start with a small number and increase it with every change you make
+  version = '1.4.7',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = "Single-cell identity definition using one vs all random forest classification and recursive feature elimination",
   long_description = "Single-cell identity definition using one vs all random forest classification and recursive feature elimination",
@@ -40,6 +40,7 @@ setup(
     'scanpy>=1.4.3',
     'pandas>=0.24.2',
     'seaborn>=0.9.0',
+    'tqdm>=4.42.1'
     ],
 
   classifiers=[
